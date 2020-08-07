8a50e7a69698bf69f4408237eb8c8098c943aab3
Lucian Olosutean <lucian.olosutean@arobs.com>
2020-08-07 16:41:23 +0300

    first commit, swagger config and first running controller

---
+++ sbw/src/main/java/com/arobs/sbw/SbwApplication.java
@@ -0,0 +1,13 @@
+package com.arobs.sbw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbwApplication.class, args);
+	}
+
+}
---
+++ sbw/src/main/java/com/arobs/sbw/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.arobs.sbw.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/", produces = "application/json")
+    public String index() {
+        return "{\"title\": \"Greetings from Spring Boot Arobs!\"}";
+    }
+}
